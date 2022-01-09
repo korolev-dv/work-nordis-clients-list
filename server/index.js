1a226f96dd61948184b75dd0a847418511906d2c
unknown <korolev_dv@list.ru>
2022-01-09 14:51:20 +0300

    init

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', clientsRoutes)
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
