867414f42e84bf06e7d850fc0cb897ae087b5200
unknown <korolev_dv@list.ru>
2022-01-28 19:50:30 +0300

    seqrch query

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import ClientsList from './components/ClientsList'
 import Navbar from './components/Navbar';
 import './styles/App.css'
@@ -7,27 +7,43 @@ import './styles/App.css'
 function App() {
 
   const [clientsList, setClientsList] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
 
   useEffect(() => {
     fetchClients()
   }, [])
-
 
   async function fetchClients() {
     const response = await axios.get("http://62.192.227.9:7000/api/getClientsList")
     setClientsList(response.data)
   }
 
+  const searchedClientsList = useMemo(() => {
+
+    if (searchQuery) {
+      return clientsList.filter(client => client.client_name.toLowerCase().includes(searchQuery))    
+    }
+    return clientsList
+ 
+  }, [clientsList, searchQuery])
+
   return (
     <div className="App">
       <Navbar />
       
       <div style={{ width: "400px" }}>
-        <input className="form-control me-2" type="search" placeholder="Найти ..." aria-label="Search"></input>
+        <input 
+            className="form-control me-2"
+            type="search" 
+            placeholder="Найти ..." 
+            aria-label="Search" 
+            value={searchQuery} 
+            onChange={e => setSearchQuery(e.target.value)}>
+        </input>
       </div>
 
       <h3 style={{ textAlign: 'center' }}>Список клиентов</h3>
-      <ClientsList clients={clientsList} />
+      <ClientsList clients={searchedClientsList} />
     </div>
   );
 }
